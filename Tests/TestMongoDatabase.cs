fd285368b8284e793aaccd02b871d15f53b4f026
lev <levtrifonov@mail.ru>
2021-11-22 15:36:25 +0500

    Первое сделано

--- Tests/TestMongoDatabase.cs
+++ Tests/TestMongoDatabase.cs
@@ -7,8 +7,9 @@ namespace Tests
     {
         public static IMongoDatabase Create()
         {
-            var mongoConnectionString = Environment.GetEnvironmentVariable("PROJECT5100_MONGO_CONNECTION_STRING")
-                                        ?? "mongodb://localhost:27017";
+            var mongoConnectionString = "mongodb+srv://lev_3fon:lev1990t@cluster0.qz5qk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+            // var mongoConnectionString = Environment.GetEnvironmentVariable("PROJECT5100_MONGO_CONNECTION_STRING")
+            //                             ?? "mongodb://localhost:27017";
             var mongoClient = new MongoClient(mongoConnectionString);
             return mongoClient.GetDatabase("game-tests");
         }
